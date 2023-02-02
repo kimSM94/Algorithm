497ef0909011581ed1f5c5dd98bf080a32ce3d1a
kimSM94 <94KimSM@gmail.com>
2023-02-02 22:51:24 +0900

    20230202 스터디

---
+++ src/main/java/S20230202/최댓값.java
@@ -0,0 +1,46 @@
+package S20230202;
+
+import java.io.*;
+import java.util.stream.IntStream;
+
+class 최댓값 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int max = 0;
+        int[] arr = new int[9];
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i < 8; i++) {
+            if (i == 0) {
+                if (arr[i] > arr[i + 1]) {
+                    max = arr[i];
+                } else {
+                    max = arr[i + 1];
+                }
+            } else {
+                if (max > arr[i + 1]) {
+                    max = max;
+                } else {
+                    max = arr[i + 1];
+                }
+
+            }
+
+
+        }
+
+        int finalMax = max;
+        int index2 = IntStream.range(0, arr.length)
+                .filter(i -> finalMax == arr[i]).findFirst().orElse(-1);
+
+        System.out.println(max+"\n"+(index2+1));
+
+
+    }
+
+}
