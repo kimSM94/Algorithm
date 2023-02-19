6feb62a654bb61bc919fc7010023cee15c8a06e4
상민김 <82505269+kimSM94@users.noreply.github.com>
2023-02-19 14:10:11 +0900

    [Silver V] Title: 색종이, Time: 76 ms, Memory: 11608 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        // arr[0][0] ~ arr[100][100] 까지 데이터 0 을 넣음
+        // arr[3][7] ~ arr[13][17] 까지 데이터 1을 넣음
+
+        int[][] arr = new int[100][100];
+        int count = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                arr[i][j] =0;
+            }
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < num; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int width  = Integer.parseInt(st.nextToken());
+            int height = Integer.parseInt(st.nextToken());
+
+            for (int j = width; j < width+10; j++) {
+
+                for (int k = height; k < height+10; k++) {
+                    arr[j][k] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if(arr[i][j] ==1){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+
+    }
+}
