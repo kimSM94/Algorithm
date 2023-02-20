9f2d303da74746bc3614be5b738c19fb993bc27a
상민김 <82505269+kimSM94@users.noreply.github.com>
2023-02-20 21:54:47 +0900

    [Bronze V] Title: 나머지, Time: 116 ms, Memory: 12864 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        System.out.println((A+B)%C);
+        System.out.println( ((A%C) + (B%C))%C);
+        System.out.println( (A*B)%C);
+        System.out.println( ((A%C) * (B%C))%C);
+
+
+    }
+}
