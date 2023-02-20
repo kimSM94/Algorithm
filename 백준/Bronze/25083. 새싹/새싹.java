a4b60058766975c895899bc6424a1726676dd068
상민김 <82505269+kimSM94@users.noreply.github.com>
2023-02-20 21:55:27 +0900

    [Bronze V] Title: 새싹, Time: 72 ms, Memory: 11304 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("         ,r'\"7\n"
+                + "r`-_   ,'  ,/\n"
+                + " \\. \". L_r'\n"
+                + "   `~\\/\n"
+                + "      |\n"
+                + "      |");
+    }
+}
