79308086ac250eb0e6fb19f9dacccfc6adb0e70c
상민김 <82505269+kimSM94@users.noreply.github.com>
2023-02-20 21:54:08 +0900

    [Bronze V] Title: ??!, Time: 76 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,21 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String lastString = "??!";
+
+        String name = br.readLine();
+
+        System.out.println(name + lastString);
+
+
+    }
+}
