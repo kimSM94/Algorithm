5a7b0962de022a4d2299acd6de30510533f005f1
S.M.KIM <82505269+kimSM94@users.noreply.github.com>
2023-03-26 22:41:32 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 76 ms, Memory: 11476 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,23 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+
+    static int MenOfPassion(int[] A, int n){
+        int i = n / 2;
+        return A[i];
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        System.out.println("1");
+        System.out.println("0");
+
+    }
+}
