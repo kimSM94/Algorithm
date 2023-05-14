7a17a8a53e77f8ef768825a2972a748f3498e73a
S.M.KIM <82505269+kimSM94@users.noreply.github.com>
2023-05-14 11:22:22 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 2404 ms, Memory: 95324 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    /*
+    * 문제 : x좌표가 증ㅈ가하는 순으로 x좌표가 같으면 y 좌표가 증가
+    * */
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int num = Integer.parseInt(br.readLine());
+
+        Integer[][] arr = new Integer[num][2];
+
+        for (int i = 0; i < num; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, new Comparator<Integer[]>() {
+            @Override
+            public int compare(Integer[] o1, Integer[] o2) {
+
+                if(!o1[1].equals(o2[1])){
+                    return o1[1]-o2[1];
+                }else{
+                    return o1[0]-o2[0];
+                    
+                }
+            }
+        });
+
+        for (int i = 0; i < num; i++) {
+            System.out.println(arr[i][0] + " "+ arr[i][1]);
+        }
+
+    }
+}
