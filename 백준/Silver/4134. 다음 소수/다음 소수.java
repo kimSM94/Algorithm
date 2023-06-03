dea33170c12ec5816f53046ab6e2a6daf91fdbd9
S.M.KIM <82505269+kimSM94@users.noreply.github.com>
2023-06-03 15:28:12 +0900

    [Silver IV] Title: 다음 소수, Time: 868 ms, Memory: 100524 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -0,0 +1,31 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < num; i++) {
+            long num2 = Long.parseLong(br.readLine());
+            var bi = new BigInteger(String.valueOf(num2));
+
+            if (bi.isProbablePrime(10)) {
+                sb.append(num2).append("\n");
+            } else {
+                sb.append(bi.nextProbablePrime()).append("\n");
+            }
+        }
+        System.out.println(sb);
+
+    }
+    // : 숫자는 음수가 아니어야 하며 너무 크지 않아야 합니다
+    // https://www.geeksforgeeks.org/biginteger-nextprobableprime-method-in-java-with-examples/
+}
+
